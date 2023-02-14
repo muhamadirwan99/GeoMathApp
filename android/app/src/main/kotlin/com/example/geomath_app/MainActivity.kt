465a7c7b88f174a953836070cac6544114d59330
Muhamad Irwan <muhamadirwan99@gmail.com>
2023-02-14 14:49:40 +0700

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/geomath_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.geomath_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/common/style.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+
+// final TextTheme myTextTheme = TextTheme(
+//   displayLarge: GoogleFonts.roboto(
+//     fontSize: 20,
+//     fontWeight: FontWeight.w700,
+//   ), //headline 1
+//   displayMedium: GoogleFonts.roboto(
+//     fontSize: 18,
+//     fontWeight: FontWeight.w600,
+//   ), //headline 2
+//   displaySmall: GoogleFonts.roboto(
+//     fontSize: 16,
+//     fontWeight: FontWeight.w600,
+//   ), //headline 3
+//   headlineMedium: GoogleFonts.roboto(
+//     fontSize: 16,
+//     fontWeight: FontWeight.w500,
+//   ), //headline 4
+//   headlineSmall: GoogleFonts.roboto(
+//     fontSize: 14,
+//     fontWeight: FontWeight.w600,
+//   ), //headline 5
+//   titleLarge: GoogleFonts.roboto(
+//     fontSize: 12,
+//     fontWeight: FontWeight.w600,
+//   ), //headline 6
+//   titleMedium: GoogleFonts.roboto(
+//     fontSize: 12,
+//     fontWeight: FontWeight.w400,
+//   ), //subtitle 1
+//   titleSmall: GoogleFonts.roboto(
+//     fontSize: 10,
+//     fontWeight: FontWeight.w400,
+//   ), //subtitle 2
+//   bodyLarge: GoogleFonts.roboto(
+//     fontSize: 16,
+//     fontWeight: FontWeight.w400,
+//   ), //body text 1
+//   bodyMedium: GoogleFonts.roboto(
+//     fontSize: 14,
+//     fontWeight: FontWeight.w400,
+//   ), //body text 2
+//   bodySmall: GoogleFonts.lato(
+//     fontSize: 14,
+//     fontWeight: FontWeight.w700,
+//   ), //caption
+// );
+
+//Primary Pallete
+const Color primaryPurple = Color(0xff5C3FCB);
+const Color primaryYellow = Color(0xffF9C14C);
+
+// Neutral Pallete
+const Color neutral50 = Color(0xffFAFAFA);
+const Color neutral100 = Color(0xffF1F1F1);
+const Color neutral150 = Color(0xffE4E4E4);
+const Color neutral200 = Color(0xffD3D3D3);
+const Color neutral300 = Color(0xffB5B5B5);
+const Color neutral400 = Color(0xff9F9F9F);
+const Color neutral500 = Color(0xff878787);
+const Color neutral600 = Color(0xff6C6C6C);
+const Color neutral700 = Color(0xff515151);
+const Color neutral800 = Color(0xff363636);
+const Color neutral850 = Color(0xff292929);
+const Color neutral900 = Color(0xff1B1B1B);
+const Color neutral950 = Color(0xff0E0E0E);
+const Color neutral1000 = Color(0xff000000);
+
+// Blue Pallete
+const Color blue50 = Color(0xffEFECFA);
+const Color blue100 = Color(0xffDED9F5);
+const Color blue150 = Color(0xffCEC5EF);
+const Color blue200 = Color(0xffBEB2EA);
+const Color blue300 = Color(0xff9D8CE0);
+const Color blue400 = Color(0xff7D65D5);
+const Color blue500 = Color(0xff5C3FCB);
+const Color blue600 = Color(0xff4A32A2);
+const Color blue700 = Color(0xff37267A);
+const Color blue800 = Color(0xff251951);
+const Color blue850 = Color(0xff1C133D);
+const Color blue900 = Color(0xff120D29);
+const Color blue1000 = Color(0xff090614);
+
+// Yellow Pallete
+const Color yellow50 = Color(0xffFEF9ED);
+const Color yellow100 = Color(0xffFEF3DB);
+const Color yellow150 = Color(0xffFDECC9);
+const Color yellow200 = Color(0xffFDE6B7);
+const Color yellow300 = Color(0xffFBDA94);
+const Color yellow400 = Color(0xffFACD70);
+const Color yellow500 = Color(0xffF9C14C);
+const Color yellow600 = Color(0xffCFA03D);
+const Color yellow700 = Color(0xffA57E2E);
+const Color yellow800 = Color(0xff7A5D1F);
+const Color yellow850 = Color(0xff654C18);
+const Color yellow900 = Color(0xff503B10);
+const Color yellow1000 = Color(0xff261A01);
+
+// Danger Pallete
+const Color danger50 = Color(0xffFFEAEA);
+const Color danger100 = Color(0xffFED5D5);
+const Color danger150 = Color(0xffFEC1C1);
+const Color danger200 = Color(0xffFEACAC);
+const Color danger300 = Color(0xffFD8282);
+const Color danger400 = Color(0xffFD5959);
+const Color danger500 = Color(0xffFC2F2F);
+const Color danger600 = Color(0xffCA2626);
+const Color danger700 = Color(0xff971C1C);
+const Color danger800 = Color(0xff651313);
+const Color danger850 = Color(0xff4C0E0E);
+const Color danger900 = Color(0xff320909);
+const Color danger1000 = Color(0xff190505);
+
+// Success Pallete
+const Color success100 = Color(0xffDEFBD5);
+const Color success200 = Color(0xffB8F7AC);
+const Color success300 = Color(0xff86E97F);
+const Color success400 = Color(0xff5BD35D);
+const Color success500 = Color(0xff2CB73C);
+const Color success600 = Color(0xff209D3A);
+const Color success700 = Color(0xff168336);
+const Color success800 = Color(0xff0E6A32);
+const Color success850 = Color(0xff292929);
+const Color success900 = Color(0xff08572E);
