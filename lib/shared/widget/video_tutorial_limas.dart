9b2ccc196eb7463b0aaee12507813133e2fc4e62
Muhamad Irwan <muhamadirwan99@gmail.com>
2023-03-08 20:17:39 +0700

    fetch video pembelajaran materi limas

--- lib/shared/widget/video_tutorial_limas.dart
+++ lib/shared/widget/video_tutorial_limas.dart
@@ -1,7 +1,11 @@
 // ignore_for_file: camel_case_types
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
 import 'package:flutter/material.dart';
 import 'package:geomath_app/common/style.dart';
+import 'package:geomath_app/core.dart';
+import 'package:skeletons/skeletons.dart';
 
 class VideoTutorialLimas extends StatefulWidget {
   const VideoTutorialLimas({
@@ -29,7 +33,9 @@ class _VideoTutorialLimasState extends State<VideoTutorialLimas> {
                 ),
               ),
               InkWell(
-                onTap: () {},
+                onTap: () {
+                  Get.to(const VideoPembelajaranView());
+                },
                 child: Text(
                   "Lainnya",
                   style: semiBold20.copyWith(
@@ -43,60 +49,102 @@ class _VideoTutorialLimasState extends State<VideoTutorialLimas> {
         const SizedBox(
           height: 16.0,
         ),
-        SizedBox(
-          height: 180,
-          child: ListView.builder(
-            itemCount: 5,
-            scrollDirection: Axis.horizontal,
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            itemBuilder: (context, index) {
+        StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection("videos")
+              .where("kd_video", isEqualTo: "1")
+              .snapshots(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (!snapshot.hasData) {
               return SizedBox(
-                width: 180,
-                child: Card(
-                  shape: RoundedRectangleBorder(
-                    side: const BorderSide(color: neutral100, width: 1),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Column(
-                    children: [
-                      ClipRRect(
-                        borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(10),
-                          topRight: Radius.circular(10),
+                height: 180,
+                child: ListView.builder(
+                  itemCount: 3,
+                  shrinkWrap: true,
+                  physics: const ScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  itemBuilder: (BuildContext context, int index) {
+                    return Row(
+                      children: [
+                        SkeletonLine(
+                          style: SkeletonLineStyle(
+                              width: 180,
+                              height: 174,
+                              borderRadius: BorderRadius.circular(10)),
                         ),
-                        child:
-                            Image.asset("assets/illustration/card_image.png"),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Matematika Dasar : Pendahuluan Limas",
-                              style: semiBold10.copyWith(color: neutral900),
-                            ),
-                            const SizedBox(
-                              height: 8.0,
-                            ),
-                            Text(
-                              "Video ini menceritakan mengenai pengenalan bentuk limas dalam BAB Geometri.",
-                              style: reguler10.copyWith(color: neutral900),
-                              maxLines: 2,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                            const SizedBox(
-                              height: 8.0,
-                            ),
-                          ],
+                        const SizedBox(
+                          width: 16.0,
                         ),
-                      ),
-                    ],
-                  ),
+                      ],
+                    );
+                  },
                 ),
               );
-            },
-          ),
+            }
+            final data = snapshot.data.docs.map((doc) => doc.data()).toList();
+
+            return SizedBox(
+              height: 180,
+              child: ListView.builder(
+                itemCount: data.length,
+                scrollDirection: Axis.horizontal,
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                itemBuilder: (context, index) {
+                  VideosResult video = VideosResult.fromJson(data[index]);
+
+                  return SizedBox(
+                    width: 180,
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        side: const BorderSide(color: neutral100, width: 1),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Column(
+                        children: [
+                          ClipRRect(
+                            borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(12),
+                              topRight: Radius.circular(12),
+                            ),
+                            child: FancyShimmerImage(
+                              imageUrl: video.thumbnail,
+                              boxFit: BoxFit.fitWidth,
+                              height: 90,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  video.title,
+                                  style: semiBold10.copyWith(color: neutral900),
+                                ),
+                                const SizedBox(
+                                  height: 8.0,
+                                ),
+                                Text(
+                                  video.desc,
+                                  style: reguler10.copyWith(color: neutral900),
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                                const SizedBox(
+                                  height: 8.0,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            );
+          },
         ),
       ],
     );
